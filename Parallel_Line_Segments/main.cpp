8eed1039077f65f0d82559790f4ee7a03af575ab
Yash Arora <yash.arora310@gmail.com>
2020-06-07 19:15:01 +0530

    Initial Commit

---
+++ Parallel_Line_Segments/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Parallel_Line_Segments
+//
+//  Created by Yash Arora on 07/06/20.
+//  Copyright © 2020 Yash Arora. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
